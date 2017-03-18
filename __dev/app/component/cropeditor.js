96386c51a54623fe027b8868bdd1d1c891440a75
Volkmar Kantor <volkmar.kantor@gmx.de>
2017-03-18 12:42:04 +0100

    make sizes selectable

--- __dev/app/component/cropeditor.js
+++ __dev/app/component/cropeditor.js
@@ -15,8 +15,23 @@ CROP_THUMBNAILS_VUE.components.cropeditor = {
 		
 		that.cropData = axios.get('http://localhost/wordpress-dev/wp-admin/admin-ajax.php?action=cpt_cropdata&imageId='+this.imageId)
 			.then(function(response) {
+				that.makeAllInactive(response.data.imageSizes);
 				that.cropData = response.data;
-				console.log('data loaded',response.data,that.cropData);
+				console.log('data loaded',response.data,that);
 			});
+	},
+	methods:{
+		toggleActive : function(image) {
+			console.log('toggleActive',image);
+			image.active = !image.active;
+		},
+		makeAllInactive : function(imageSizes) {
+			if(imageSizes===undefined) {
+				imageSizes = this.cropData.imageSizes;
+			}
+			for(var prop in imageSizes) {
+				imageSizes[prop].active = false;
+			}
+		}
 	}
 };
