5acb7005ca2ee5d31d2623158f26488481267daa
Volkmar Kantor <volkmar.kantor@gmx.de>
2021-01-01 16:03:32 +0100

    cleanup app folder

--- __dev/vue.config.js
+++ __dev/vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   outputDir: './../app/',
   runtimeCompiler: true,
-  filenameHashing: false
+
+  // disable hashes in filenames
+  filenameHashing: false,
+  // delete HTML related webpack plugins
+  chainWebpack: config => {
+    config.plugins.delete('html')
+    config.plugins.delete('preload')
+    config.plugins.delete('prefetch')
+  }
 }
